a8a3d727009b1c0fd0a3fe369b902e607b5ceed9
ubogdanovic <bogdanovic.u98@gmail.com>
2021-05-22 12:31:30 +0200

    Brisanje usera

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -67,7 +67,7 @@ class UserController extends Controller
     public function destroy($id)
     {
         $user = User::find($id);
-        $user->destroy();
+        $user->destroy($id);
     }
     
     
